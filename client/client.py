d35f49ee1084c0f159e6900d97c375bfc4af9309
yuanhao10 <atgcaugc10@gmail.com>
2015-01-09 03:39:08 +0800

    v1.0

---
+++ client/client.py
@@ -0,0 +1,302 @@
+import sys
+import socket
+import random
+import thread
+from PyQt4 import QtCore, QtGui
+
+SERVER_PORT = 8081
+MAXBUFF = 1024
+HOST_NAME = 'localhost'
+HOST_ADDR = '127.0.0.1'
+
+STATUS_ERROR = -1
+STATUS_IDLE = 0
+STATUS_BLUE_TURN = 1
+STATUS_RED_TURN = 2
+STATUS_BLUE_WIN = 3
+STATUS_RED_WIN = 4
+STATUS_DRAW = 5
+STATUS_FULL = 6
+
+def randomPortNum():
+	return random.randint(8082, 8999)
+
+def setCharAt(tar, c, i):
+	return tar[:i] + c + tar[(i+1):] # unsafe
+
+
+class MyWindow(QtGui.QMainWindow):
+
+	def __init__(self):
+		super(MyWindow, self).__init__()
+		self.setWindowTitle('Nanashi')
+
+		self.label1 = QtGui.QLabel('NANASHI Game v1.0 By Hao\nRight Click for the Menu')
+		self.label1.setAlignment(QtCore.Qt.AlignCenter)
+		self.setCentralWidget(self.label1)
+
+		self.popMenu = QtGui.QMenu()
+		newgameaction = QtGui.QAction('New Game', self)
+		coughaction = QtGui.QAction('Cough', self)
+		quitgameaction = QtGui.QAction('Quit Game', self)
+		self.popMenu.addAction(newgameaction)
+		self.popMenu.addAction(coughaction)
+		self.popMenu.addAction(quitgameaction)
+		self.connect(newgameaction, QtCore.SIGNAL('triggered()'), self.onNewGame)
+		self.connect(coughaction, QtCore.SIGNAL('triggered()'), self.onCough)
+		self.connect(quitgameaction, QtCore.SIGNAL('triggered()'), self.onQuitGame)
+
+		self.chessPositions = '0000000003000030003113000020020000200200003113000300003000000000'
+		self.gameStatus = STATUS_IDLE
+		self.side = None
+		self.activePos = None
+		self.cSocket = None
+
+		self.recvFlag = False
+		self.dThreadDone = False
+		self.dThread = thread.start_new_thread(self.listen2server, ())
+
+		self.initUI()
+
+	def initUI(self):
+		self.setGeometry(300, 150, 400, 400)
+		self.setWindowFlags(QtCore.Qt.WindowMinimizeButtonHint)
+		self.setFixedSize(self.width(), self.height())
+		self.show() # repaint window
+
+	def initSocket(self):
+		while True:
+			try:
+				cPort = randomPortNum() # in (8082, 8999)
+				self.cSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+				self.cSocket.bind(('', cPort))
+				print 'success: [initSocket] port number is %d' % cPort
+				break
+			except:
+				print 'error: [initSocket] port number taken. retrying...'
+				continue
+		return
+
+	def listen2server(self):
+		if (self.cSocket is None):
+			self.initSocket() # init udp socket
+		while (not self.dThreadDone):
+			data = addr = None
+			try:
+				data, addr = self.cSocket.recvfrom(MAXBUFF)
+				self.recvFlag = True
+				print 'Received:', data, '--- From', addr
+				self.onServerMsg(data, addr)
+			except:
+				continue
+		return
+
+	def paintEvent(self, e):
+		qp = QtGui.QPainter()
+		qp.begin(self)
+		self.drawChess(qp)
+		qp.end()
+
+	def onNewGame(self):
+		if (self.gameStatus != STATUS_IDLE):
+			return
+		self.label1.setText('') # hide center label
+		self.setWindowTitle('Nanashi\t(Connecting...)')
+		data = addr = None
+		# while True:
+		# 	if (self.recvFlag):
+		# 		self.recvFlag = False
+		# 		break
+		# 	self.cSocket.sendto('NG', (HOST_NAME, SERVER_PORT))
+		self.cSocket.sendto('NG', (HOST_NAME, SERVER_PORT))
+		# self.onServerMsg(data, addr)
+
+	def onCough(self):
+		self.cSocket.sendto('$', (HOST_NAME, SERVER_PORT))
+
+	def onQuitGame(self):
+		self.dThreadDone = True
+		self.cSocket.sendto('#', (HOST_NAME, SERVER_PORT))
+		sys.exit()
+
+	def mousePressEvent(self, event):
+		if (event.button() != QtCore.Qt.LeftButton):
+			return
+		x = event.x()
+		y = event.y()
+		# print x, y
+		# self.side = 1
+		if (self.gameStatus == STATUS_IDLE):
+			print 'idle left click'
+			return
+		print 'left clicked'
+		if ((self.gameStatus == STATUS_BLUE_TURN) and (self.side == 1)):
+			return
+		if ((self.gameStatus == STATUS_RED_TURN) and (self.side == 0)):
+			return
+		clickPos = x / 50 * 8 + y / 50
+		if (int(self.chessPositions[clickPos]) == 3): # rock
+			return
+		elif (int(self.chessPositions[clickPos]) == (1-self.side)+1): # opponent
+			return
+		elif (int(self.chessPositions[clickPos]) == self.side+1): # self
+			self.activePos = clickPos # activate position
+			self.update()
+			return
+		# on a blank
+		if (self.activePos is None): # not active
+			return
+		# already activated
+		tempCb = self.updateChessBoard(clickPos)
+		# while True:
+		# 	if (self.recvFlag):
+		# 		self.recvFlag = False
+		# 		break
+		# 	self.cSocket.sendto(tempCb, (HOST_NAME, SERVER_PORT))
+		self.cSocket.sendto(tempCb, (HOST_NAME, SERVER_PORT))
+		# self.onServerMsg(data, addr)
+
+	# when click on a blank with an active position
+	def updateChessBoard(self, clickPos):
+		res = self.chessPositions
+		# if it is a jump
+		if ((abs(clickPos-self.activePos) == 16) or ((clickPos/8 == self.activePos/8) and (abs(clickPos-self.activePos) == 2))):
+			res = setCharAt(res, '0', self.activePos)
+		elif ((abs(clickPos/8-self.activePos/8) > 1) or (abs(clickPos%8-self.activePos%8) > 1)):
+			return res
+		# put a new chess in the blank
+		res = setCharAt(res, str(self.side+1), clickPos)
+		# change the color of adjacent chess
+		tar = []
+		if ((clickPos % 8 != 0) and (res[clickPos-1] != '3') and (res[clickPos-1] != '0')): # up
+			tar.append(clickPos-1)
+		if ((clickPos / 8 != 0) and (res[clickPos-8] != '3') and (res[clickPos-8] != '0')): # left
+			tar.append(clickPos-8)
+		if ((clickPos % 8 != 7) and (res[clickPos+1] != '3') and (res[clickPos+1] != '0')): # down
+			tar.append(clickPos+1)
+		if ((clickPos / 8 != 7) and (res[clickPos+8] != '3') and (res[clickPos+8] != '0')): # right
+			tar.append(clickPos+8)
+		if ((clickPos/8 > 0) and (clickPos%8 > 0) and (res[clickPos-9] != '3') and (res[clickPos-9] != '0')): # upleft
+			tar.append(clickPos-9)
+		if ((clickPos/8 > 0) and (clickPos%8 < 7) and (res[clickPos-7] != '3') and (res[clickPos-7] != '0')): # downleft
+			tar.append(clickPos-7)
+		if ((clickPos/8 < 7) and (clickPos%8 > 0) and (res[clickPos+7] != '3') and (res[clickPos+7] != '0')): # upright
+			tar.append(clickPos+7)
+		if ((clickPos/ 8 < 7) and (clickPos%8 < 7) and (res[clickPos+9] != '3') and (res[clickPos+9] != '0')): # downright
+			tar.append(clickPos+9)
+		for _ in tar:
+			res = setCharAt(res, str(self.side+1), _)
+		return res
+
+	def onServerMsg(self, data, addr):
+		dataArr = data.strip().strip('@').split('@')
+		status = int(dataArr[0])
+		if (self.gameStatus == STATUS_IDLE):
+			if (status == STATUS_ERROR):
+				self.setWindowTitle('Nanashi\t(Server error.)')
+			elif (status == STATUS_FULL):
+				self.setWindowTitle('Nanashi\t(Server busy. Try later.)')
+			elif (status == STATUS_IDLE):
+				self.setWindowTitle('Nanashi\t(Connected. Waiting for opponent.)')
+			elif (status == STATUS_BLUE_TURN): # start new game
+				side = dataArr[1]
+				cb = dataArr[2]
+				self.chessPositions = cb # set chessboard
+				self.side = int(side) # set color
+				if (self.side == 0): # check my color
+					self.setWindowTitle('Nanashi\t(Blue. Your turn.)')
+				else:
+					self.setWindowTitle('Nanashi\t(Red. Opponent turn.)')
+				self.gameStatus = status
+				self.update()
+			else:
+				self.setWindowTitle('Nanashi\t(Unidentified error.)')
+		elif ((self.gameStatus == STATUS_BLUE_TURN) or (self.gameStatus == STATUS_RED_TURN)):
+			if (status == STATUS_ERROR):
+				self.setWindowTitle('Nanashi\t(Server error.)')
+				self.gameStatus = STATUS_IDLE
+				self.activePos = None
+			elif (status == STATUS_BLUE_TURN): # now blue turn
+				cb = dataArr[2]
+				self.chessPositions = cb # update chessboard
+				if (self.side == 0): # check my color
+					self.setWindowTitle('Nanashi\t(Blue. Your turn.)')
+				else:
+					self.setWindowTitle('Nanashi\t(Red. Opponent turn.)')
+				self.gameStatus = status # update status
+				self.activePos = None
+				self.update()
+			elif (status == STATUS_RED_TURN): # now red turn
+				cb = dataArr[2]
+				self.chessPositions = cb # update chessboard
+				if (self.side == 0): # check my color
+					self.setWindowTitle('Nanashi\t(Blue. Opponent turn.)')
+				else:
+					self.setWindowTitle('Nanashi\t(Red. Your turn.)')
+				self.gameStatus = status # update status
+				self.activePos = None
+				self.update()
+			elif (status == STATUS_BLUE_WIN): # winner is blue
+				cb = dataArr[2]
+				self.chessPositions = cb # update chessboard
+				if (self.side == 0): # check my color
+					self.setWindowTitle('Nanashi\t(Blue. You win!)')
+				else:
+					self.setWindowTitle('Nanashi\t(Red. You lose!)')
+				self.gameStatus = STATUS_IDLE # update status
+				self.activePos = None
+				self.update()
+			elif (status == STATUS_RED_WIN): # winner is red
+				cb = dataArr[2]
+				self.chessPositions = cb # update chessboard
+				if (self.side == 0): # check my color
+					self.setWindowTitle('Nanashi\t(Blue. You lose!)')
+				else:
+					self.setWindowTitle('Nanashi\t(Red. You win!)')
+				self.gameStatus = STATUS_IDLE # update status
+				self.activePos = None
+				self.update()
+			elif (status == STATUS_DRAW): # draw game
+				cb = dataArr[2]
+				self.chessPositions = cb # update chessboard
+				if (self.side == 0): # check my color
+					self.setWindowTitle('Nanashi\t(Blue. Draw game!)')
+				else:
+					self.setWindowTitle('Nanashi\t(Red. Draw game!)')
+				self.gameStatus = STATUS_IDLE # update status
+				self.activePos = None
+				self.update()
+
+	def contextMenuEvent(self, event):
+		self.popMenu.exec_(event.globalPos())
+
+	def drawChess(self, qp):
+		for i in xrange(0, len(self.chessPositions)):
+			boardX = i / 8
+			boardY = i % 8
+			posX = boardX * 50
+			posY = boardY * 50
+			if (self.chessPositions[i] == '0'):
+				brush = QtGui.QBrush(QtGui.QColor(255, 255, 255))
+			elif (self.chessPositions[i] == '1'):
+				if (i == self.activePos):
+					brush = QtGui.QBrush(QtGui.QColor(100, 100, 255))
+				else:
+					brush = QtGui.QBrush(QtGui.QColor(0, 0, 255))
+			elif (self.chessPositions[i] == '2'):
+				if (i == self.activePos):
+					brush = QtGui.QBrush(QtGui.QColor(255, 100, 100))
+				else:
+					brush = QtGui.QBrush(QtGui.QColor(255, 0, 0))
+			elif (self.chessPositions[i] == '3'):
+				brush = QtGui.QBrush(QtGui.QColor(0, 0, 0))
+			else:
+				self.dThreadDone = True
+				sys.exit()
+			qp.setBrush(brush)
+			qp.drawRect(posX, posY, 50, 50)
+
+
+app = QtGui.QApplication(sys.argv)
+mywindow = MyWindow()
+app.exec_()
---
+++ server/server.py
@@ -0,0 +1,310 @@
+import socket
+import random
+import time
+
+STATUS_ERROR = -1
+STATUS_IDLE = 0
+STATUS_BLUE_TURN = 1
+STATUS_RED_TURN = 2
+STATUS_BLUE_WIN = 3
+STATUS_RED_WIN = 4
+STATUS_DRAW = 5
+STATUS_FULL = 6
+
+JUDGEMENT_PLAYING = 0
+JUDGEMENT_BLUE = 1
+JUDGEMENT_RED = 2
+JUDGEMENT_DRAW = 3
+
+PORT = 8081
+MAXBUFF = 1024
+
+TABLENUM = 4
+IP_TABLE = [[0, 0], [0, 0], [0, 0], [0, 0]]
+CHESSBOARD_TABLE = ['', '', '', '']
+STATUS_TABLE = [0, 0, 0, 0]
+TIME_TABLE = [0, 0, 0, 0]
+EXPIRE_TIME = 5
+
+timestone = 0
+timestoneFlag = False
+
+MAPNUM = 12
+MAPS = [
+	'1000000000000000000000000000000000000000000000000000000000000002',
+	'1000000000000000000000000003300000033000000000000000000000000002',
+	'1000000000000000000330000030030000300300000330000000000000000002',
+	'0000000000000000000330000031230000321300000330000000000000000000',
+	'1000000200300300033003300000000000000000033003300030030020000001',
+	'0000000000000000000110000003300000033000000220000000000000000000',
+	'3000000330000003130000321300003213000032130000323000000330000003',
+	'1000000000000000000330000000300000030000000330000000000000000002',
+	'0000000000000000000030000001330000332000000300000000000000000000',
+	'0000000003000030003113000020020000200200003113000300003000000000',
+	'0000000000000000003003000003320000133000003003000000000000000000',
+	'0000000000000000003003000233331000300300003003000003300000000000'
+]
+
+
+def randomChessBoard():
+	return MAPS[random.randint(0, MAPNUM-1)]
+
+def updateChessBoard(data, table):
+	global CHESSBOARD_TABLE
+	CHESSBOARD_TABLE[table] = data
+
+def checkTimeout():
+	global timestone
+	global timestoneFlag
+	global IP_TABLE
+	global CHESSBOARD_TABLE
+	global STATUS_TABLE
+	global TIME_TABLE
+
+	print 'checking timeout...'
+
+	if (timestoneFlag == False):
+		timestoneFlag = True
+		timestone = time.time()
+	timenow = time.time()
+	telapse = timenow - timestone
+	timestone = timenow
+	for table in xrange(0, TABLENUM):
+		TIME_TABLE[table] += telapse
+		if (STATUS_TABLE[table] != STATUS_IDLE):
+			if (TIME_TABLE[table] > EXPIRE_TIME):
+				tempStatus = -1
+				if (STATUS_TABLE[table] == STATUS_BLUE_TURN):
+					tempStatus = STATUS_RED_WIN
+				else:
+					tempStatus = STATUS_BLUE_WIN
+				# declare result
+				infoBlue = str(tempStatus) + '@0@' + CHESSBOARD_TABLE[table]
+				infoRed = str(tempStatus) + '@1@' + CHESSBOARD_TABLE[table]
+				soc.sendto(infoBlue, IP_TABLE[table][0])
+				soc.sendto(infoRed, IP_TABLE[table][1])
+				# clear game
+				IP_TABLE[table] = [0, 0]
+				CHESSBOARD_TABLE[table] = ''
+				STATUS_TABLE[table] = STATUS_IDLE
+				print 'Table %d: Time out. Status: %d' % (table, tempStatus)
+
+
+def judgeGame(gamestr):
+	blueNum = redNum = 0
+	blankFlag = False
+	for i in xrange(0, len(gamestr)):
+		if (gamestr[i] == '0'):
+			blankFlag = True
+		elif (gamestr[i] == '1'):
+			blueNum += 1
+		elif (gamestr[i] == '2'):
+			redNum += 1
+	if (blankFlag == True):
+		if (blueNum == 0):
+			return JUDGEMENT_RED
+		if (redNum == 0):
+			return JUDGEMENT_RED
+		return JUDGEMENT_PLAYING
+	elif (blueNum > redNum):
+		return JUDGEMENT_BLUE
+	elif (blueNum < redNum):
+		return JUDGEMENT_RED
+	else:
+		return JUDGEMENT_DRAW
+
+# main part
+
+soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+soc.bind(('', PORT))
+print 'UDP LISTENING...'
+
+while True:
+	checkTimeout()
+
+	data, addr = soc.recvfrom(MAXBUFF)
+	print 'Received:', data, ' --- From', addr
+
+	# check cough
+	if (data == '$'):
+		continue
+
+	table, side, flag = -1, -1, False
+	emptyTable, emptySide, emptyFlag = -1, -1, False
+	# traverse IP_TABLE
+	for i in xrange(0, TABLENUM):
+		if ((table != -1) or (emptyTable != -1)):
+			break
+		for j in (0, 1):
+			if (IP_TABLE[i][j] == addr):
+				table, side, flag = i, j, True
+				break
+			if (IP_TABLE[i][j] == 0):
+				emptyTable, emptySide, emptyFlag = i, j, True
+				break
+	
+	# check if ip exists
+	if (flag == True):
+		# check if opponent exists
+		if (IP_TABLE[table][1-side] == 0):
+			# if it is a quit msg
+			if (data.strip() == '#'):
+				soc.sendto('#', addr) # quit ack
+				IP_TABLE[table] = [0, 0]
+				print 'He quit. He is %d.' % side
+				continue
+			# tell him again to wait
+			infoWait = str(STATUS_IDLE) + '@' + str(side)
+			soc.sendto(infoWait, addr)
+			print 'IP exists. But no opponent.'
+			continue
+		else:
+			# if it is a quit msg
+			if (data.strip() == '#'):
+				# tell the opponent
+				if (side == 0):
+					tempStatus = STATUS_RED_WIN
+				else:
+					tempStatus = STATUS_BLUE_WIN
+				infoOppo = str(tempStatus) + '@' + str(1-side) + '@' + CHESSBOARD_TABLE[table]
+				soc.sendto(infoOppo, IP_TABLE[table][1-side])
+				# clear game
+				IP_TABLE[table] = [0, 0]
+				CHESSBOARD_TABLE[table] = ''
+				STATUS_TABLE[table] = STATUS_IDLE
+				print 'He quit. He is %d.' % side
+				continue
+			# check game status
+			if (STATUS_TABLE[table] == STATUS_IDLE):
+				# init new game
+				TIME_TABLE[table] = 0
+				CHESSBOARD_TABLE[table] = randomChessBoard()
+				infoBlue = str(STATUS_BLUE_TURN) + '@0@' + CHESSBOARD_TABLE[table]
+				infoRed = str(STATUS_BLUE_TURN) + '@1@' + CHESSBOARD_TABLE[table]
+				soc.sendto(infoBlue, IP_TABLE[table][0])
+				soc.sendto(infoRed, IP_TABLE[table][1])
+				STATUS_TABLE[table] = STATUS_BLUE_TURN
+				print 'Table %d: New Game Started (THIS NOT GONNA HAPPEN)' % table
+			elif (STATUS_TABLE[table] == STATUS_BLUE_TURN):
+				if (side == 0):
+					# update timetable
+					TIME_TABLE[table] = 0
+					# parse data, analyse and judge
+					updateChessBoard(data, table) # update directly
+					res = judgeGame(CHESSBOARD_TABLE[table]) # judge result
+					if (res == JUDGEMENT_PLAYING): # still playing
+						infoBlue = str(STATUS_RED_TURN) + '@0@' + CHESSBOARD_TABLE[table]
+						infoRed = str(STATUS_RED_TURN) + '@1@' + CHESSBOARD_TABLE[table]
+						soc.sendto(infoBlue, IP_TABLE[table][0])
+						soc.sendto(infoRed, IP_TABLE[table][1])
+						STATUS_TABLE[table] = STATUS_RED_TURN
+						print 'Table %d: the Blue updated' % table
+					else: # game end
+						tempStatus = -1
+						if (res == JUDGEMENT_BLUE):
+							tempStatus = STATUS_BLUE_WIN
+						elif (res == JUDGEMENT_RED):
+							tempStatus = STATUS_RED_WIN
+						else: # draw
+							tempStatus = STATUS_DRAW
+						# declare result
+						infoBlue = str(tempStatus) + '@0@' + CHESSBOARD_TABLE[table]
+						infoRed = str(tempStatus) + '@1@' + CHESSBOARD_TABLE[table]
+						soc.sendto(infoBlue, IP_TABLE[table][0])
+						soc.sendto(infoRed, IP_TABLE[table][1])
+						# clear game
+						IP_TABLE[table] = [0, 0]
+						CHESSBOARD_TABLE[table] = ''
+						STATUS_TABLE[table] = STATUS_IDLE
+						print 'Table %d: the Blue updated. GAME END. Status: %d' % (table, tempStatus)
+				else:
+					# ignore data
+					infoRed = str(STATUS_BLUE_TURN) + '@1@' + CHESSBOARD_TABLE[table]
+					soc.sendto(infoRed, IP_TABLE[table][1])
+					print 'Not his turn. Blue turn. He is red.'
+					continue
+			elif (STATUS_TABLE[table] == STATUS_RED_TURN):
+				if (side == 1):
+					# update timetable
+					TIME_TABLE[table] = 0
+					# parse data, analyse and judge
+					updateChessBoard(data, table) # update directly
+					res = judgeGame(CHESSBOARD_TABLE[table]) # judge result
+					if (res == JUDGEMENT_PLAYING): # still playing
+						infoBlue = str(STATUS_BLUE_TURN) + '@0@' + CHESSBOARD_TABLE[table]
+						infoRed = str(STATUS_BLUE_TURN) + '@1@' + CHESSBOARD_TABLE[table]
+						soc.sendto(infoBlue, IP_TABLE[table][0])
+						soc.sendto(infoRed, IP_TABLE[table][1])
+						STATUS_TABLE[table] = STATUS_BLUE_TURN
+						print 'Table %d: the Red updated' % table
+					else: # end game
+						tempStatus = -1
+						if (res == JUDGEMENT_BLUE):
+							tempStatus = STATUS_BLUE_WIN
+						elif (res == JUDGEMENT_RED):
+							tempStatus = STATUS_RED_WIN
+						else: # draw
+							tempStatus = STATUS_DRAW
+						# declare result
+						infoBlue = str(tempStatus) + '@0@' + CHESSBOARD_TABLE[table]
+						infoRed = str(tempStatus) + '@1@' + CHESSBOARD_TABLE[table]
+						soc.sendto(infoBlue, IP_TABLE[table][0])
+						soc.sendto(infoRed, IP_TABLE[table][1])
+						# clear game
+						IP_TABLE[table] = [0, 0]
+						CHESSBOARD_TABLE[table] = ''
+						STATUS_TABLE[table] = STATUS_IDLE
+						print 'Table %d: the Red updated. GAME END. Status: %d' % (table, tempStatus)
+				else:
+					# ignore data
+					infoBlue = str(STATUS_RED_TURN) + '@0@' + CHESSBOARD_TABLE[table]
+					soc.sendto(infoBlue, IP_TABLE[table][0])
+					print 'Not his turn. Red turn. He is blue.'
+					continue
+			else: # error handler
+				try:
+					infoBlue = str(STATUS_ERROR) + '@0@' + CHESSBOARD_TABLE[table]
+					infoRed = str(STATUS_ERROR) + '@1@' + CHESSBOARD_TABLE[table]
+					soc.sendto(infoBlue, IP_TABLE[table][0])
+					soc.sendto(infoRed, IP_TABLE[table][1])
+					TIME_TABLE[table] = 0
+					IP_TABLE[table] = [0, 0]
+					CHESSBOARD_TABLE[table] = ''
+					STATUS_TABLE[table] = STATUS_IDLE
+				except:
+					print 'Game system crashed. Sorry.'
+					sys.exit()
+	else:
+		# if it is a quit msg
+		if (data.strip() == '#'):
+			# ignore
+			print 'He quit. He is not logged in.'
+			continue
+		if (emptyFlag == True):
+			# put into empty position
+			IP_TABLE[emptyTable][emptySide] = addr
+			# check if opponent exists
+			if (IP_TABLE[emptyTable][1-emptySide] == 0):
+				# tell client to wait opponent
+				infoWait = str(STATUS_IDLE) + '@' + str(emptySide)
+				soc.sendto(infoWait, addr)
+				print 'Sit into Table %d Side %d. Waiting for opponent.' % (emptyTable, emptySide)
+			else:
+				# init new game
+				TIME_TABLE[emptyTable] = 0
+				CHESSBOARD_TABLE[emptyTable] = randomChessBoard()
+				infoBlue = str(STATUS_BLUE_TURN) + '@0@' + CHESSBOARD_TABLE[emptyTable]
+				infoRed = str(STATUS_BLUE_TURN) + '@1@' + CHESSBOARD_TABLE[emptyTable]
+				print IP_TABLE[emptyTable]
+				soc.sendto(infoBlue, IP_TABLE[emptyTable][0])
+				soc.sendto(infoRed, IP_TABLE[emptyTable][1])
+				STATUS_TABLE[emptyTable] = STATUS_BLUE_TURN
+				print 'Table %d: New Game Started (THIS GONNA HAPPEN)' % emptyTable
+		else:
+			# tell client to retry after a while
+			infoFull = str(STATUS_FULL) + '@0'
+			soc.sendto(infoFull, addr)
+			print 'He had no table to sit down.'
+
+print 'What happend?'
+sys.exit()
